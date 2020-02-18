bab74ac28fd1309a88deba3afca1c1b125cc4fd9
Matthew Chuah <mchuah2003@yahoo.com.sg>
2020-02-18 20:35:45 +1100

    Setup Table View

---
+++ Emo_Learn/Emo_Learn/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Emo_Learn
+//
+//  Created by Matthew Chuah on 18/2/20.
+//  Copyright © 2020 Matthew Chuah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    @IBOutlet weak var MyTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
